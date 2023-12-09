4632b603996c5c1a978c3dc5127eb186fd9fb063
rubyciide <joelpaul900@gmail.com>
2023-12-09 11:14:22 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// http://api.weatherapi.com/v1/current.json?key=dfecc53d1ca344578de35309230912&q=
+
+//
+const giphyKey = '&api_key=CrpHODR2TAesMkW7XQc3ZAV0BcfOvLpk'
+
+const url = 'http://api.giphy.com/v1/gifs/search?q=funny+cat'
+import './styles/main.css';
+import axios from "axios"
+const button = document.getElementById('getWeatherBtn');
+const input = document.getElementById('city');
+button.addEventListener('click', () => {
+    getWeather(input.value);
+
+})
+
+function getWeather(city) {
+    const results = document.getElementById('results');
+    const result = document.getElementById('result')
+    showLoading();
+    const url = "http://api.weatherapi.com/v1/current.json?key=dfecc53d1ca344578de35309230912&q=";
+    axios.get(url + city).then(res => {
+        document.getElementById('result').textContent = res.data.current.feelslike_c + " deg C";
+        const img = document.getElementById('weatherImg');
+        img.src = res.data.current.condition.icon;
+        fetch('https://api.giphy.com/v1/gifs/search?api_key=CrpHODR2TAesMkW7XQc3ZAV0BcfOvLpk&q=rainy+day&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips').then(
+            res => res.json()).then(data => {
+                console.log(data);
+                document.body.style.backgroundImage = `url('${data.data[Math.floor(Math.random(10) * 10)].images.original.url}')`;
+
+            })
+    }).catch(err => console.log(err))
+}
+
+function showLoading() {
+    result.textContent = "loading..."
+}
+
+// fetch('http://api.weatherapi.com/v1/current.json?key=dfecc53d1ca344578de35309230912&q=' + "mananthavady").then(res => res.json()).then(data => {
+//     document.getElementById('results').textContent = JSON.stringify(data)
+//     console.log(data)
+
+// }
+// )
