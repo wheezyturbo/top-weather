442878c79cef2db36ec26de369ef9c4512880a18
rubyciide <joelpaul900@gmail.com>
2023-12-09 16:52:59 +0530

    [ADD] more weather details

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// http://api.weatherapi.com/v1/current.json?key=dfecc53d1ca344578de35309230912&q=
-
-//
 const giphyKey = '&api_key=CrpHODR2TAesMkW7XQc3ZAV0BcfOvLpk'
 
 const url = 'http://api.giphy.com/v1/gifs/search?q=funny+cat'
@@ -8,37 +5,86 @@ import './styles/main.css';
 import axios from "axios"
 const button = document.getElementById('getWeatherBtn');
 const input = document.getElementById('city');
+const results = document.getElementById('results');
+const result = document.getElementById('result');
+const weatherImg = document.getElementById('weatherImg');
+
 button.addEventListener('click', () => {
     getWeather(input.value);
-
-})
+});
 
 function getWeather(city) {
-    const results = document.getElementById('results');
-    const result = document.getElementById('result')
+    const d = document.querySelector('.details');
+    const c = document.querySelector('.condition');
+    console.log(c, d);
+    if (d) d.remove()
+    if (c) c.remove();
+    weatherImg.src = "";
     showLoading();
-    const url = "http://api.weatherapi.com/v1/current.json?key=dfecc53d1ca344578de35309230912&q=";
-    axios.get(url + city).then(res => {
-        const search = res.data.current.condition.text;
-        document.getElementById('result').textContent = res.data.current.feelslike_c + " deg C";
-        const img = document.getElementById('weatherImg');
-        img.src = res.data.current.condition.icon;
-        fetch(`https://api.giphy.com/v1/gifs/search?api_key=CrpHODR2TAesMkW7XQc3ZAV0BcfOvLpk&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`).then(
-            res => res.json()).then(data => {
-                console.log(data);
-                document.body.style.backgroundImage = `url('${data.data[Math.floor(Math.random(10) * 10)].images.fixed_width_small.url}')`;
+    const url = `http://api.weatherapi.com/v1/current.json?key=dfecc53d1ca344578de35309230912&q=${city}`;
 
-            })
-    }).catch(err => console.log(err))
+    axios.get(url)
+        .then(res => {
+            document.querySelector('.loading').remove();
+            const weatherData = res.data.current;
+
+            // Update temperature and condition
+            result.textContent = `${weatherData.temp_c}°C`;
+            weatherImg.src = weatherData.condition.icon;
+
+            // Create elements for additional details
+            const details = document.createElement('div');
+            details.classList.add('details');
+
+            const farenheit = document.createElement('h5');
+            farenheit.textContent = `${weatherData.temp_f}°F`;
+
+            const humidity = document.createElement('p');
+            humidity.textContent = `Humidity: ${weatherData.humidity}%`;
+
+            const wind = document.createElement('p');
+            wind.textContent = `Wind: ${weatherData.wind_kph} km/h, ${weatherData.wind_dir}`;
+
+            const condition = document.createElement('p');
+            condition.classList.add('condition');
+            condition.textContent = weatherData.condition.text;
+
+            // Append details to the card
+            details.appendChild(farenheit);
+            details.appendChild(humidity);
+            details.appendChild(wind);
+            results.appendChild(details);
+            document.querySelector('.weatherBox').appendChild(condition);
+
+            // Update background based on weather condition
+            const searchTerm = weatherData.condition.text;
+            fetch(`https://api.giphy.com/v1/gifs/search?${giphyKey}&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+                .then(res => res.json())
+                .then(data => {
+                    document.body.style.backgroundImage = `url('${data.data[0].images.fixed_width_small.url}')`;
+                });
+        })
+        .catch(err => {
+            result.textContent = 'Error fetching weather data';
+            console.error(err);
+        });
 }
+
+function getWeatherFromCoords() {
+    let latitude, longitude;
+    navigator.geolocation.getCurrentPosition(pos => {
+        const { coords } = pos;
+        console.log(coords);
+        latitude = coords.latitude;
+        longitude = coords.longitude;
+    })
+    console.log(latitude, longitude);
+}
+getWeatherFromCoords();
 
 function showLoading() {
-    result.textContent = "loading..."
+    const animation = document.createElement('div');
+    animation.classList.add('loading');
+    result.textContent = '';
+    result.appendChild(animation);
 }
-
-// fetch('http://api.weatherapi.com/v1/current.json?key=dfecc53d1ca344578de35309230912&q=' + "mananthavady").then(res => res.json()).then(data => {
-//     document.getElementById('results').textContent = JSON.stringify(data)
-//     console.log(data)
-
-// }
-// )
