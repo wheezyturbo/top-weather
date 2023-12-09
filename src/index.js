5f652182b1e98512a7e87dfd4592e3fcc9c1891f
rubyciide <joelpaul900@gmail.com>
2023-12-09 11:21:31 +0530

    [UPDATE] randomize gifs based data.current.condition.text

--- src/index.js
+++ src/index.js
@@ -19,13 +19,14 @@ function getWeather(city) {
     showLoading();
     const url = "http://api.weatherapi.com/v1/current.json?key=dfecc53d1ca344578de35309230912&q=";
     axios.get(url + city).then(res => {
+        const search = res.data.current.condition.text;
         document.getElementById('result').textContent = res.data.current.feelslike_c + " deg C";
         const img = document.getElementById('weatherImg');
         img.src = res.data.current.condition.icon;
-        fetch('https://api.giphy.com/v1/gifs/search?api_key=CrpHODR2TAesMkW7XQc3ZAV0BcfOvLpk&q=rainy+day&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips').then(
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=CrpHODR2TAesMkW7XQc3ZAV0BcfOvLpk&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`).then(
             res => res.json()).then(data => {
                 console.log(data);
-                document.body.style.backgroundImage = `url('${data.data[Math.floor(Math.random(10) * 10)].images.original.url}')`;
+                document.body.style.backgroundImage = `url('${data.data[Math.floor(Math.random(10) * 10)].images.fixed_width_small.url}')`;
 
             })
     }).catch(err => console.log(err))
